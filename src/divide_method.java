b21097578f9b2d794a94da4dcbe9654a17496174
Miregul <140221804+Miregul@users.noreply.github.com>
2023-11-09 12:19:36 -0500

    this is for divide_method

---
+++ src/divide_method.java
@@ -0,0 +1,10 @@
+public class divide_method {
+    public static void main(String[] args) {
+        divide(2,4);
+    }
+
+    public static void  divide(double num1 , double num2){
+
+        System.out.println(num1/num2);
+    }
+}
