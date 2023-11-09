820ec1bd0df6a48e5d30689a894b326df0258995
Miregul <140221804+Miregul@users.noreply.github.com>
2023-11-09 12:08:58 -0500

    first code  for odd & even

--- src/week01/OddOrEven_Admir.java
+++ src/week01/OddOrEven_Admir.java
@@ -2,7 +2,18 @@ package week01;
 
 public class OddOrEven_Admir {
     public static void main(String[] args) {
-        System.out.println("just stared ");
+
+       odd_or_even(6);
+       odd_or_even(5);
+
+
+
     }
-    
+    public static void odd_or_even(int number){
+        if (number%2==0){
+            System.out.println(number+" is even number");
+        }else {
+            System.out.println(number+ " is odd number");
+        }
+    }
 }
