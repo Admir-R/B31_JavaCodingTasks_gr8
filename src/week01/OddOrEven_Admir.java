fa6506494549ad229b5d601ab805c9b55d19bc14
Admir Rapo <admir.rapo@gmail.com>
2023-11-10 18:24:10 -0500

    update

--- src/week01/OddOrEven_Admir.java
+++ src/week01/OddOrEven_Admir.java
@@ -2,7 +2,7 @@ package week01;
 
 public class OddOrEven_Admir {
     public static void main(String[] args) {
-        System.out.println("just stared ");
+        System.out.println("just started ");
     }
     
 }
