e42225e0345027be2d5998cc2148b0a2995a20b9
Admir Rapo <admir.rapo@gmail.com>
2023-11-14 18:48:19 -0500

    Created OddOrEven Method

--- src/week01/OddOrEven_Admir.java
+++ src/week01/OddOrEven_Admir.java
@@ -2,7 +2,16 @@ package week01;
 
 public class OddOrEven_Admir {
     public static void main(String[] args) {
-        System.out.println("just started ");
+        System.out.println( identify(90));
+    }
+
+
+    public static String  identify(int number){
+        if(number % 2 == 0){
+            return   "Even";
+        }else{
+            return "Odd";
+        }
     }
     
 }
