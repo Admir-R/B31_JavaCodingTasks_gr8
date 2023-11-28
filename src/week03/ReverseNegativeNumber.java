53bbe53dec8d7d325a766aa5b2cf5e03720f2de7
sahika <sahikagurler@gmail.com>
2023-11-28 14:33:42 +0300

    reverse a negative number method

--- src/week03/ReverseNegativeNumber.java
+++ src/week03/ReverseNegativeNumber.java
@@ -1,21 +1,25 @@
 package week03;
 
 public class ReverseNegativeNumber {
-    public static void main(String[] args) {
 
-        int num = -215;
+    public static int ReverseNumber(int num) {
 
+        int reversed = 0;
 
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
 
-
-
-
-
-
-
-
-
-
-
+        return reversed;
     }
 }
+
+
+
+
+
+
+
+
