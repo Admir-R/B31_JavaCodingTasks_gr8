d7773c02ab584e0171762006cf354b29209d2419
sahika <sahikagurler@gmail.com>
2023-11-28 13:48:59 +0300

    PrimeNumber method created

--- src/week03/PrimeNumber.java
+++ src/week03/PrimeNumber.java
@@ -1,9 +1,16 @@
 package week03;
-
 public class PrimeNumber {
-    public static void main(String[] args) {
 
+    public static void Prime(int num) {
 
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.println("Not a Prime Number");
+            }
+        }
+        System.out.println("Prime Number");
+
+    }
 
 
 
@@ -15,4 +22,4 @@ public class PrimeNumber {
 
 
     }
-}
+
