b909b84dd6a3b61a8fab75cf15c7e95315239e48
SamA <samuelagasi@gmail.com>
2023-12-02 02:37:38 -0500

    Added week 3 tasks

--- src/week_03/ReverseNegativeNumbers.java
+++ src/week_03/ReverseNegativeNumbers.java
@@ -2,12 +2,26 @@ package week_03;
 
 public class ReverseNegativeNumbers {
     public static void main(String[] args) {
-        System.out.println(ReverseNegative(-21));
+        System.out.println(ReverseNegative(-321));
 
     }
     public static int ReverseNegative(int number){
-        int positiveNum= number*(-1);
-        return positiveNum;
+
+        boolean negNum=number<0;
+        if (negNum){
+            number=-number;
+        }
+
+
+        int temp=0;
+        while (number>0){
+            temp=temp*10 + number%10;
+            number=number/10;
+        }
+        if (negNum){
+            temp=-temp;
+        }
+        return temp;
     }
 }
 /*
